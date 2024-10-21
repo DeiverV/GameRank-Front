5cc911cda88ce919dae3d2695953c0cc0c14a7b6
DeiberV <deiberveranodev@gmail.com>
2024-10-21 18:47:43 -0500

    feat(auth): userSlice interface

--- src/store/slices/users.slice.ts
+++ src/store/slices/users.slice.ts
@@ -1,22 +1,37 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { LoginResponse } from "../models";
 
 interface SliceState {
   token: string;
+  username: string;
+  email: string;
+  name: string;
+  id: string;
 }
 
 const initialState: SliceState = {
   token: "",
+  username: "",
+  email: "",
+  name: "",
+  id: "",
 };
 
 export const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    login: (state, action: PayloadAction<LoginResponse>) => {
+    login: (state, action: PayloadAction<SliceState>) => {
+      state.email = action.payload.email;
+      state.name = action.payload.name;
+      state.username = action.payload.username;
+      state.id = action.payload.id;
       state.token = action.payload.token;
     },
     logout: (state) => {
+      state.email = "";
+      state.name = "";
+      state.username = "";
+      state.id = "";
       state.token = "";
     },
   },
